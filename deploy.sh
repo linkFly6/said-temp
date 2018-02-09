e91579d2276372d9d8f37a3d0c83b47d221a7b25
jianglai <jianglai@didichuxing.com>
2018-02-10 03:02:18 +0800

    部署脚本优化

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,9 @@ echo '============= deploy start ============='
 npm i --production
 
 export NODE_ENV=production
+export PORT=80
 cpu_num=`cat /proc/cpuinfo| grep "processor"| wc -l`
 # 根据 cpu 个数进行集群部署
 echo "cpu sums: $cpu_num, env: $env)"
-pm2 stop $prog
-pm2 start ./.app.js --name $prog -i $cpu_num
+pm2 delete $prog
+pm2 start ./server.js --name $prog -i $cpu_num
--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ const app = express();
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
-app.set("views", path.join(__dirname, "../views"));
+app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "pug");
 app.use(compression());
 app.use(bodyParser.json());
